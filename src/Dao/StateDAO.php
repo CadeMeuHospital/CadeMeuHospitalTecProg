5d562868cc0b282f210063bb222d7812893f9844
Fagner <fanig_01@hotmail.com>
2014-04-02 18:37:17 -0300

    Applied style pattern in StateDAO class

--- src/Dao/StateDAO.php
+++ src/Dao/StateDAO.php
@@ -1,37 +1,37 @@
 <?php
 
-require_once "/../Utils/DataBaseConnection.php";
+require_once '/../Utils/DataBaseConnection.php';
 
-class StateDAO {
-
+class StateDAO
+{
     private static $instanceStateDAO;
-    
-    private function __construct() {
-        
+
+    private function __construct()
+    {
     }
 
-    public static function getInstanceStateDAO() {
-
+    public static function getInstanceStateDAO()
+    {
         if (!isset(self::$instanceStateDAO)) {
             self::$instanceStateDAO = new StateDAO();
         }
 
         return self::$instanceStateDAO;
     }
-    
-    public function saveAverageEvaluationStateDAO($evaluate, $stateAcronym) {
-        
-        $sqlAvgCount = "SELECT SUM(ubs.average) AS average, 
+
+    public function saveAverageEvaluationStateDAO($evaluate, $stateAcronym)
+    {
+        $sqlAvgCount = "SELECT SUM(ubs.average) AS average,
                         COUNT(ubs.average) AS quantity FROM ubs
-                        INNER JOIN state, municipios_ibge 
+                        INNER JOIN state, municipios_ibge
                         WHERE state.acronym = municipios_ibge.uf
-                        AND state.acronym = '" . $stateAcronym . "' 
-                        AND municipios_ibge.codigo = ubs.cod_munic 
+                        AND state.acronym = '" . $stateAcronym . "'
+                        AND municipios_ibge.codigo = ubs.cod_munic
                         AND ubs.average <> 0";
 
         $resultAvgCount = mysql_query($sqlAvgCount);
         $arrayAvgCount = mysql_fetch_row($resultAvgCount);
-        
+
         if ($arrayAvgCount[1] > 0) {
             $average = ($arrayAvgCount[0]) / ($arrayAvgCount[1]);
             $sql = "UPDATE state SET average = '" . $average . "' WHERE acronym = '" . $stateAcronym . "'";
@@ -40,25 +40,28 @@ class StateDAO {
         }
 
         $result = mysql_query($sql);
+
         return $result;
     }
 
-    public function takeUfStateUBS($codMunic) {
+    public function takeUfStateUBS($codMunic)
+    {
         $sql = "SELECT uf FROM municipios_ibge WHERE codigo LIKE '" . $codMunic . "'";
         $result = mysql_query($sql);
         $state = mysql_fetch_row($result);
+
         return $state;
     }
 
-    public function takeStateUBSOO($codMunic) {
+    public function takeStateUBSOO($codMunic)
+    {
         $ufState = $this->takeUfStateUBS($codMunic);
-        
+
         $sql = "SELECT * FROM state WHERE acronym LIKE '" . $ufState[0] . "'";
         $result = mysql_query($sql);
         $state = mysql_fetch_row($result);
+
         return $state;
     }
 
 }
-
-?>
