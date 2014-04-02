0dfe1bc32ed8427668bdf41905cfe0a74f78ed6f
Fagner <fanig_01@hotmail.com>
2014-04-02 18:42:40 -0300

    Applied style pattern in City class

--- src/Model/City.php
+++ src/Model/City.php
@@ -1,44 +1,48 @@
 <?php
 
-class City {
-    
+class City
+{
     private $codMunic;
     private $dscCidade;
-    
+
     private $state;
-            
-    function __construct($codMunic, $dscCidade, $state) {
+
+    public function __construct($codMunic, $dscCidade, $state)
+    {
         $this->setCodMunic($codMunic);
         $this->setDscCidade($dscCidade);
-        
+
         $this->setState($state);
     }
-    
-    public function getState() {
+
+    public function getState()
+    {
         return $this->state;
     }
 
-    public function setState($state) {
+    public function setState($state)
+    {
         $this->state = $state;
     }
-    
-    public function getCodMunic() {
+
+    public function getCodMunic()
+    {
         return $this->codMunic;
     }
 
-    public function setCodMunic($codMunic) {
+    public function setCodMunic($codMunic)
+    {
         $this->codMunic = $codMunic;
     }
 
-    public function getDscCidade() {
+    public function getDscCidade()
+    {
         return $this->dscCidade;
     }
 
-    public function setDscCidade($dscCidade) {
+    public function setDscCidade($dscCidade)
+    {
         $this->dscCidade = $dscCidade;
     }
-    
-    
-}
 
-?>
+}
