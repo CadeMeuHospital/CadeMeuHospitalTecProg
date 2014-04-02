e8f63527d7191e7516396bbd206ed2b97080e728
Fagner <fanig_01@hotmail.com>
2014-04-02 18:44:22 -0300

    Applied style pattern in ProfileUBS class

--- src/Model/ProfileUBS.php
+++ src/Model/ProfileUBS.php
@@ -1,7 +1,7 @@
 <?php
 
-class ProfileUBS {
-
+class ProfileUBS
+{
     private $idUBS;
     private $latitudeUBS;
     private $longitudeUBS;
@@ -14,12 +14,12 @@ class ProfileUBS {
     private $descriTools;
     private $descMedicine;
     private $average;
-    
+
     private $city;
 
-    function __construct($idUBS, $latitudeUBS, $longitudeUBS, $codCNES,
-            $nameUBS, $descEnder, $phoneUBS,$physicStructureUBS, 
-            $adapOldPeople, $descriTools, $descMedicine, 
+    public function __construct($idUBS, $latitudeUBS, $longitudeUBS, $codCNES,
+            $nameUBS, $descEnder, $phoneUBS,$physicStructureUBS,
+            $adapOldPeople, $descriTools, $descMedicine,
             $average, $city) {
         $this->setIdUBS($idUBS);
         $this->setLatitudeUBS($latitudeUBS);
@@ -33,114 +33,138 @@ class ProfileUBS {
         $this->setDescriTools($descriTools);
         $this->setDescMedicine($descMedicine);
         $this->setAverage($average);
-        
+
         $this->setCity($city);
     }
-    
-    public function getCity(){
+
+    public function getCity()
+    {
         return $this->city;
     }
 
-    public function setCity($city){
+    public function setCity($city)
+    {
         $this->city = $city;
     }
-    
-    public function getIdUBS(){
+
+    public function getIdUBS()
+    {
         return $this->idUBS;
     }
 
-    public function setIdUBS($idUBS){
+    public function setIdUBS($idUBS)
+    {
         $this->idUBS = $idUBS;
     }
 
-    public function getLatitudeUBS(){
+    public function getLatitudeUBS()
+    {
         return $this->latitudeUBS;
     }
 
-    public function setLatitudeUBS($latitudeUBS){
+    public function setLatitudeUBS($latitudeUBS)
+    {
         $this->latitudeUBS = $latitudeUBS;
     }
 
-    public function getLongitudeUBS(){
+    public function getLongitudeUBS()
+    {
         return $this->longitudeUBS;
     }
 
-    public function setLongitudeUBS($longitudeUBS){
+    public function setLongitudeUBS($longitudeUBS)
+    {
         $this->longitudeUBS = $longitudeUBS;
     }
 
-    public function getCodCNES(){
+    public function getCodCNES()
+    {
         return $this->codCNES;
     }
 
-    public function setCodCNES($codCNES){
+    public function setCodCNES($codCNES)
+    {
         $this->codCNES = $codCNES;
     }
 
-    public function getNameUBS() {
+    public function getNameUBS()
+    {
         return $this->nameUBS;
     }
 
-    public function setNameUBS($nameUBS){
+    public function setNameUBS($nameUBS)
+    {
         $this->nameUBS = $nameUBS;
     }
 
-    public function getDescEnder(){
+    public function getDescEnder()
+    {
         return $this->descEnder;
     }
 
-    public function setDescEnder($descEnder){
+    public function setDescEnder($descEnder)
+    {
         $this->descEnder = $descEnder;
     }
 
-    public function getPhoneUBS(){
+    public function getPhoneUBS()
+    {
         return $this->phoneUBS;
     }
 
-    public function setPhoneUBS($phoneUBS){
+    public function setPhoneUBS($phoneUBS)
+    {
         $this->phoneUBS = $phoneUBS;
     }
 
-    public function getPhysicStructureUBS(){
+    public function getPhysicStructureUBS()
+    {
         return $this->physicStructureUBS;
     }
 
-    public function setPhysicStructureUBS($physicStructureUBS){
+    public function setPhysicStructureUBS($physicStructureUBS)
+    {
         $this->physicStructureUBS = $physicStructureUBS;
     }
 
-    public function getAdapOldPeople(){
+    public function getAdapOldPeople()
+    {
         return $this->adapOldPeople;
     }
 
-    public function setAdapOldPeople($adapOldPeople){
+    public function setAdapOldPeople($adapOldPeople)
+    {
         $this->adapOldPeople = $adapOldPeople;
     }
 
-    public function getDescriTools(){
+    public function getDescriTools()
+    {
         return $this->descriTools;
     }
 
-    public function setDescriTools($descriTools){
+    public function setDescriTools($descriTools)
+    {
         $this->descriTools = $descriTools;
     }
 
-    public function getDescMedicine() {
+    public function getDescMedicine()
+    {
         return $this->descMedicine;
     }
 
-    public function setDescMedicine($descMedicine){
+    public function setDescMedicine($descMedicine)
+    {
         $this->descMedicine = $descMedicine;
     }
 
-    public function setAverage($average){
+    public function setAverage($average)
+    {
         $this->average = $average;
     }
 
-    public function getAverage(){
+    public function getAverage()
+    {
         return $this->average;
     }
 
 }
-
-?>
