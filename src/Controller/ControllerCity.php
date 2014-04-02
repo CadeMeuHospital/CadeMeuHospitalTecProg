b586ccc85739d6ba6c9c5b5ddd6c7381d858eda6
Fagner <fagner@fagner-RV411.(none)>
2014-04-02 18:11:33 -0300

    Aply style patern in ControllerCity class

--- src/Controller/ControllerCity.php
+++ src/Controller/ControllerCity.php
@@ -3,34 +3,34 @@
 require_once '/../Dao/CityDAO.php';
 require_once '/../Model/City.php';
 
-class ControllerCity {
-    
+class ControllerCity
+{
     private static $instanceControllerCity;
 
-    private function __construct() {
-        
+    private function __construct()
+    {
     }
 
     //Singleton pattern
-    public static function getInstanceControllerCity() {
-        
+    public static function getInstanceControllerCity()
+    {
         if (!isset(self::$instanceControllerCity)) {
             self::$instanceControllerCity = new ControllerCity();
         }
+
         return self::$instanceControllerCity;
     }
-    
+
     //Making a City Object
-    public function makeObjectCity($codMunic, $state){
+    public function makeObjectCity($codMunic, $state)
+    {
         $cityDao = CityDAO::getInstanceCityDAO();
         $attributeCity = $cityDao->takeCityDatabase($codMunic);
-        
+
         $nameCity = $attributeCity[0];
-        
+
         $city = new City($codMunic, $nameCity, $state);
-        
+
         return $city;
     }
 }
-
-?>
