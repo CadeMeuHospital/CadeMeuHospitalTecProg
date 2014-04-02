b07075efe4d8a4a5fda729dc8bf223d8fa8f6190
Fagner <fanig_01@hotmail.com>
2014-04-02 18:22:22 -0300

    Applied style pattern in ControllerState class

--- src/Controller/ControllerState.php
+++ src/Controller/ControllerState.php
@@ -3,53 +3,59 @@
 require_once '/../Dao/StateDAO.php';
 require_once '/../Model/State.php';
 
-class ControllerState {
-
+class ControllerState
+{
     private static $instanceControllerState;
 
-    private function __construct() {
-        
+    private function __construct()
+    {
     }
 
     //Singleton Pattern
-    public static function getInstanceControllerState() {
+    public static function getInstanceControllerState()
+    {
         if (!isset(self::$instanceControllerState)) {
             self::$instanceControllerState = new ControllerState();
-        } 
+        }
+
         return self::$instanceControllerState;
     }
-    
+
     //Saving the avarage evaluation
-    public function saveAverageEvaluationState($evaluate, $stateAcronym) {
+    public function saveAverageEvaluationState($evaluate, $stateAcronym)
+    {
         $stateDAO = StateDAO::getInstanceStateDAO();
+
         return $stateDAO->saveAverageEvaluationStateDAO($evaluate, $stateAcronym);
     }
-    
+
     //Taking a State
-    public function takeState($codMunic) {
+    public function takeState($codMunic)
+    {
         $stateDAO = StateDAO::getInstanceStateDAO();
         $stateAcronym = $stateDAO->takeUfStateUBS($codMunic);
+
         return $stateAcronym;
     }
 
     //Making a State Object
-    public function makeObjectState($codMunic){
+    public function makeObjectState($codMunic)
+    {
         $stateDAO = StateDAO::getInstanceStateDAO();
-        
+
         $attributeState = $stateDAO->takeStateUBSOO($codMunic);
-        
+
         $acronym = $attributeState[1];
         $amountUBS = $attributeState[2];
         $average = $attributeState[3];
         $name = $attributeState[4];
         $population = $attributeState[5];
         $area = $attributeState[6];
-        
+
         $newState = new State($acronym, $amountUBS,
                 $average, $name, $population, $area);
-        
+
         return $newState;
     }
 
 }
-?>
