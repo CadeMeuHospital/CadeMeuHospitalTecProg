b18fa3a8322fc6dea318afd3e2444e6df1fc49be
Fagner <fanig_01@hotmail.com>
2014-04-02 18:35:21 -0300

    Applied style pattern in ProfileUBSDAO class

--- src/Dao/ProfileUBSDAO.php
+++ src/Dao/ProfileUBSDAO.php
@@ -1,23 +1,23 @@
 <?php
 
-require_once "/../Utils/DataBaseConnection.php";
-require_once "/../Utils/DataValidation.php";
+require_once '/../Utils/DataBaseConnection.php';
+require_once '/../Utils/DataValidation.php';
 
 define('NOME', 1);
 define('CIDADE', 2);
 define('ESTADO', 4);
 
-class ProfileUBSDAO {
-
+class ProfileUBSDAO
+{
     private static $instanceProfileUBSDAO;
 
-    private function __construct() {
-        
+    private function __construct()
+    {
     }
 
     //Singleton Pattern
-    public static function getInstanceProfileUBSDAO() {
-
+    public static function getInstanceProfileUBSDAO()
+    {
         if (!isset(self::$instanceProfileUBSDAO)) {
             self::$instanceProfileUBSDAO = new ProfileUBSDAO();
         }
@@ -26,23 +26,27 @@ class ProfileUBSDAO {
     }
 
     //Returning a UBS with that id
-    public function returnUBS($idUBS) {
+    public function returnUBS($idUBS)
+    {
         $sql = " SELECT * FROM ubs WHERE cod_unico LIKE '" . $idUBS . "'";
         $result = mysql_query($sql);
+
         return $result;
     }
 
     //Searching a UBS in Table evaluate
-    public function searchUBSInTableEvaluate($idUBS) {
-        $sql = "SELECT * FROM evaluate WHERE id_cod_unico 
+    public function searchUBSInTableEvaluate($idUBS)
+    {
+        $sql = "SELECT * FROM evaluate WHERE id_cod_unico
                 LIKE '" . $idUBS . "'";
         $execute = mysql_query($sql);
         $result = mysql_fetch_row($execute);
+
         return $result;
     }
 
-    public function saveEvaluationUBS($evaluate, $idUBS) {
-
+    public function saveEvaluationUBS($evaluate, $idUBS)
+    {
         $returnConsult = self::searchUBSInTableEvaluate($idUBS);
         if (!$returnConsult) {
             $sql = "INSERT INTO evaluate (id_cod_unico, amount_people,
@@ -61,16 +65,17 @@ class ProfileUBSDAO {
         return self::executeComandSQL($sql, $idUBS);
     }
 
-    public function executeComandSQL($sql, $idUBS) {
+    public function executeComandSQL($sql, $idUBS)
+    {
         mysql_query($sql);
         $evaluateAverage = self::updateEvaluateAverage($idUBS);
 
         return $evaluateAverage;
     }
 
-    public function updateEvaluateAverage($idUBS) {
-
-        $sql = "SELECT * FROM evaluate WHERE id_cod_unico 
+    public function updateEvaluateAverage($idUBS)
+    {
+        $sql = "SELECT * FROM evaluate WHERE id_cod_unico
                 LIKE '" . $idUBS . "'";
         $result = mysql_query($sql);
         $resultAverage = mysql_fetch_row($result);
@@ -81,37 +86,41 @@ class ProfileUBSDAO {
 
         $evaluateAverage = $resultAverage[3] / $resultAverage[2];
 
-        $sql = "UPDATE ubs SET average= '" . $evaluateAverage . "' 
+        $sql = "UPDATE ubs SET average= '" . $evaluateAverage . "'
                 WHERE cod_unico LIKE '" . $idUBS . "'";
         mysql_query($sql);
+
         return $evaluateAverage;
     }
 
-    public function takeAverageUBS($idUBS) {
-        $sql = "SELECT (average) FROM ubs WHERE cod_unico 
+    public function takeAverageUBS($idUBS)
+    {
+        $sql = "SELECT (average) FROM ubs WHERE cod_unico
                 LIKE '" . $idUBS . "'";
         $result = mysql_query($sql);
         $average = mysql_fetch_row($result);
+
         return $average;
     }
 
-    public function searchUBSinDatabase($field, $searchType) {
+    public function searchUBSinDatabase($field, $searchType)
+    {
         $field = trim($field);
 
         DataValidation::throwTextFieldException($field);
 
         switch ($searchType) {
             case NOME :
-                $sql = "SELECT * FROM ubs WHERE nom_estab 
+                $sql = "SELECT * FROM ubs WHERE nom_estab
                         LIKE '%" . $field . "%'";
                 break;
             case ESTADO :
-                $sql = "SELECT * FROM ubs INNER JOIN municipios_ibge 
-                        ON ubs.cod_munic = municipios_ibge.codigo 
+                $sql = "SELECT * FROM ubs INNER JOIN municipios_ibge
+                        ON ubs.cod_munic = municipios_ibge.codigo
                         WHERE municipios_ibge.uf LIKE '%" . $field . "%'";
                 break;
             case CIDADE :
-                $sql = "SELECT * FROM ubs WHERE dsc_cidade 
+                $sql = "SELECT * FROM ubs WHERE dsc_cidade
                         LIKE '%" . $field . "%'";
                 break;
         }
@@ -121,5 +130,3 @@ class ProfileUBSDAO {
     }
 
 }
-
-?>
