550f3b30d0eb55a11d24acfd7ae62ba37a51712e
Fagner <fanig_01@hotmail.com>
2014-04-02 18:36:26 -0300

    Applied style pattern in RankingDAO class

--- src/Dao/RankingDAO.php
+++ src/Dao/RankingDAO.php
@@ -1,31 +1,31 @@
 <?php
 
-require_once "/../Utils/DataBaseConnection.php";
+require_once '/../Utils/DataBaseConnection.php';
 
-class RankingDAO {
-
+class RankingDAO
+{
     private static $instanceRankingDAO;
 
-    private function __construct() {
-        
+    private function __construct()
+    {
     }
 
-    public static function getInstanceRankingDAO() {
-
+    public static function getInstanceRankingDAO()
+    {
         if (!isset(self::$instanceRankingDAO)) {
             self::$instanceRankingDAO = new RankingDAO();
         }
 
         return self::$instanceRankingDAO;
     }
-    
-    public function getRank() {
-        $sql = "SELECT nom_estab,cod_unico,average FROM ubs INNER JOIN evaluate 
-                ON ubs.cod_unico = evaluate.id_cod_unico WHERE average > 0 
+
+    public function getRank()
+    {
+        $sql = "SELECT nom_estab,cod_unico,average FROM ubs INNER JOIN evaluate
+                ON ubs.cod_unico = evaluate.id_cod_unico WHERE average > 0
                 ORDER BY average DESC,amount_people DESC LIMIT 5";
         $result = mysql_query($sql);
+
         return $result;
     }
 }
-
-?>
