4568f9ab82967ea33814bcc59e5aaa3674d2bb80
Fagner <fanig_01@hotmail.com>
2014-04-02 18:38:39 -0300

    Applied style pattern in StatisticsDAO class

--- src/Dao/StatisticsDAO.php
+++ src/Dao/StatisticsDAO.php
@@ -1,13 +1,13 @@
 <?php
 
-require_once "/../Utils/DataBaseConnection.php";
+require_once '/../Utils/DataBaseConnection.php';
 
-class StatisticsDAO {
-    
+class StatisticsDAO
+{
     private static $instanceStatisticsDAO;
 
-    public static function getInstanceStatisticsDAO() {
-
+    public static function getInstanceStatisticsDAO()
+    {
         if (!isset(self::$instanceStatisticsDAO)) {
             self::$instanceStatisticsDAO = new StatisticsDAO();
         }
@@ -15,28 +15,30 @@ class StatisticsDAO {
         return self::$instanceStatisticsDAO;
     }
 
-    public function getValuesToChartAverageEvaluate() {
-
+    public function getValuesToChartAverageEvaluate()
+    {
         $query = "SELECT SUM(amount_people_1),SUM(amount_people_2),
-            SUM(amount_people_3), 
+            SUM(amount_people_3),
             SUM(amount_people_4),SUM(amount_people_5) FROM evaluate";
         $result = mysql_query($query);
         $sum = mysql_fetch_row($result);
+
         return $sum;
     }
 
-    public function getValuesToChartAverageEvaluateSingleUBS($idUBS) {
-
+    public function getValuesToChartAverageEvaluateSingleUBS($idUBS)
+    {
         $query = "SELECT amount_people_1,amount_people_2,amount_people_3,
-            amount_people_4,amount_people_5 FROM evaluate 
+            amount_people_4,amount_people_5 FROM evaluate
             WHERE id_cod_unico='" . $idUBS . "'";
         $result = mysql_query($query);
         $chartValue = mysql_fetch_row($result);
+
         return $chartValue;
     }
 
-    public function getStatisticsByState() {
-
+    public function getStatisticsByState()
+    {
         $statistics = array();
         $names = array();
         $averages = array();
@@ -45,7 +47,7 @@ class StatisticsDAO {
         $areas = array();
 
         for ($i = 1; $i < 28; $i++) {
-            $stats = "SELECT name, amount_ubs, average, 
+            $stats = "SELECT name, amount_ubs, average,
                     population, area FROM state WHERE id_state = '" . $i . "'";
             $result = mysql_query($stats);
             $return = mysql_fetch_row($result);
@@ -65,5 +67,3 @@ class StatisticsDAO {
     }
 
 }
-
-?>
