ee0d2693d8f7c9e76319cec6f0e9deedd8dbed55
Fagner <fanig_01@hotmail.com>
2014-04-02 18:24:50 -0300

    Applied style pattern in ControllerStatistics class

--- src/Controller/ControllerStatistics.php
+++ src/Controller/ControllerStatistics.php
@@ -2,42 +2,48 @@
 
 require_once '/../Dao/StatisticsDAO.php';
 
-class ControllerStatistics {
-
+class ControllerStatistics
+{
     private static $instanceControllerStatistics;
 
-    private function __construct() {
-        
+    private function __construct()
+    {
     }
 
     //Singleton Pattern
-    public static function getInstanceControllerStatistics() {
+    public static function getInstanceControllerStatistics()
+    {
         if (!isset(self::$instanceControllerStatistics)) {
             self::$instanceControllerStatistics = new ControllerStatistics();
-        } 
+        }
+
         return self::$instanceControllerStatistics;
     }
 
     //Generatin values to evaluation avarage
-    public function generateValuesToChartAverageEvaluate() {
+    public function generateValuesToChartAverageEvaluate()
+    {
         $statisticsDAO = StatisticsDAO::getInstanceStatisticsDAO();
         $allEvaluates = $statisticsDAO->getValuesToChartAverageEvaluate();
+
         return $allEvaluates;
     }
 
     //Generating values to single UBS evaluation avarage
-    public function generateValuesToChartAverageEvaluateSingleUBS($idUBS) {
+    public function generateValuesToChartAverageEvaluateSingleUBS($idUBS)
+    {
         $statisticsDAO = StatisticsDAO::getInstanceStatisticsDAO();
         $evaluatesUBS = $statisticsDAO->getValuesToChartAverageEvaluateSingleUBS($idUBS);
+
         return $evaluatesUBS;
     }
 
     //Generating statistics of quantity avarage
-    public function generateStatisticsOfQuantityAverage() {
+    public function generateStatisticsOfQuantityAverage()
+    {
         $statisticsDAO = StatisticsDAO::getInstanceStatisticsDAO();
         $statistics = $statisticsDAO->getStatisticsByState();
+
         return $statistics;
     }
 }
-
-?>
