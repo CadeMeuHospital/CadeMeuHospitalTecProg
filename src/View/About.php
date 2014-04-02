e0df558fbfcc4b9e0798fff380b3d8ce5276c7c4
Fagner <fanig_01@hotmail.com>
2014-04-02 19:51:33 -0300

    Applied style pattern in About class

--- src/View/About.php
+++ src/View/About.php
@@ -11,23 +11,23 @@
     </head>
 
     <body align="center">
-        <div class="root">                   
-            <?php 
+        <div class="root">
+            <?php
                 require '../View/Shared/Header.php';
-                require '../View/Shared/Navigation_bar.php'; 
+                require '../View/Shared/Navigation_bar.php';
             ?>
             <div align ="center">
                 <div class="content">
-                    <table border = "1">                    
+                    <table border = "1">
                         <b><font size="6">Lei de Acesso à Informação (LAI)</font></b>
                         <table width="760"border="0">
-                           
-                            <font size="05"><p align="justify">    
-                                    Segundo o <a href="http://www.acessoainformacao.gov.br/"  target="_blank"> 
+
+                            <font size="05"><p align="justify">
+                                    Segundo o <a href="http://www.acessoainformacao.gov.br/"  target="_blank">
                                 site oficial</a>, a LAI regulamenta o direito, previsto
                                 na Constituição, de qualquer pessoa solicitar e receber dos órgãos e
                                 entidades públicos, de todos os entes e Poderes, informações públicas
-                                por eles produzidas ou custodiadas. Através da mesma, é possível se obter 
+                                por eles produzidas ou custodiadas. Através da mesma, é possível se obter
                                 dados (processados ou não), que podem ser utilizados para produção e transmissão
                                 de conhecimento, registrados em qualquer suporte ou formato.
                                </p></font>
@@ -38,23 +38,23 @@
                         <table width="760"border="0">
                            <font size="05"><p align="justify">
                                    O propósito da criação das UBS é atender aproximadamente 80% dos pacientes
-                                com problemas de saúde, que não são tão graves e não necessitam ser 
+                                com problemas de saúde, que não são tão graves e não necessitam ser
                                 encaminhados para o hospital, sendo assim, o fluxo de pacientes, que apresentam
-                                um quadro mais grave, nos hospitais deverá reduzir consideravelmente, elas 
-                                fazem parte da Política Nacional de Urgência e Emergência, lançado em 2013 
-                                pelo Governo Federal. Preferencialmente, as UBS são construídas nas cidades 
-                                mais pobres, terão prioridade os municípios que participam do Programa Brasil 
+                                um quadro mais grave, nos hospitais deverá reduzir consideravelmente, elas
+                                fazem parte da Política Nacional de Urgência e Emergência, lançado em 2013
+                                pelo Governo Federal. Preferencialmente, as UBS são construídas nas cidades
+                                mais pobres, terão prioridade os municípios que participam do Programa Brasil
                                 Sem Miséria. Nas UBS os usuários do SUS podem realizar consultas médicas básicas,
-                                como por exemplo, Pediatria, Ginecologia com exames de gravidez muito mais rápido, 
-                                aproximadamente em 5 minutos já sai o resultado, 
+                                como por exemplo, Pediatria, Ginecologia com exames de gravidez muito mais rápido,
+                                aproximadamente em 5 minutos já sai o resultado,
                                 diferentemente dos exames convencionais
-                                que levam dias para sair o resultado, Odontologia, Clinica Geral com curativos simples, 
+                                que levam dias para sair o resultado, Odontologia, Clinica Geral com curativos simples,
                                 tomar vacinas e coletar exames laboratoriais
                                </p></font>
                         </table>
                 </div>
                 <br /><br />
-                    <a href="http://dados.gov.br/dataset/unidades-basicas-de-saude-ubs" 
+                    <a href="http://dados.gov.br/dataset/unidades-basicas-de-saude-ubs"
                        target="_blank"><p align="right">Saiba Mais</p></a>
                 <br /><br /><br /><br /><br />
                 <?php require '../View/Shared/Footer.php'; ?>
